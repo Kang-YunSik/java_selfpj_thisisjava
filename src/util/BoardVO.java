3daf996f6efa7324f635071fe656bbd78d2f3b23
kys <kang_ys_1@naver.com>
2023-11-03 00:14:16 +0900

    Comment: 생성자의 개념 추가

--- src/util/BoardVO.java
+++ src/util/BoardVO.java
@@ -10,6 +10,15 @@ public class BoardVO {
     private String bwriter;
     private Date bdate;
 
+
+    // [생성자의 개념]
+    // 1. 생성자는 필드의 값을 불러와 새로운 값으로 초기화하는 역할을 합니다.
+    // 2. 이 때, 기본 생성자는 매개변수가 없는 메소드이며 필드를 해당 자료형의 기본값으로 초기화합니다.
+    //    예를 들어, int 필드는 0으로 초기화됩니다.
+    // 3. 매개변수가 있는 생성자는 호출하려면 BoardVO boardVO = new BoardVO(동일한 매개변수) 로 한다.
+    // 4. 만약, 동일한 매개변수를 받지만 다른 기능을 수행하는 메소드를 정의하고 싶다면, 메소드 명을 다르게 해야 한다.
+
+
     // 매개변수가 5개 모두 필요한 메소드
     public BoardVO(int bno, String btitle, String bcontent, String bwriter, Date bdate) {
         this.bno = bno;
